8feaec4cdfd3f86b78a671937074c7697ba381a7
aminaa181 <aomercevic1@etf.unsa.ba>
2026-01-31 23:29:23 +0100

    Update ARP_Resolver_TB_Scenario2.vhdl

--- Code/simulation/modelsim/ARP_Resolver_TB_Scenario2.vhdl
+++ Code/simulation/modelsim/ARP_Resolver_TB_Scenario2.vhdl
@@ -232,14 +232,13 @@ begin
             report "ERROR: Module did not return to IDLE state!" 
             severity error;
         
-        -- Simulation complete
-        report "=== Scenario 1 (Successful Resolution) Test Completed ===" 
-            severity note;
+ 
         
             
         sim_done <= true;
         wait;
         
     end process;
+
 
 end Behavioral;
